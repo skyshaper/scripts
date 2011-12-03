b7976275d525b4ef72691ee1a123fe2a74154eab
Daniel Friesel <derf@finalrewind.org>
2011-12-03 21:54:26 +0100

    out-wii: report battery state, need to wait after connecting

--- ledanim/out-wii.c
+++ ledanim/out-wii.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <bluetooth/bluetooth.h>
 #include <cwiid.h>
@@ -7,15 +8,25 @@ int main()
 {
 	char buf[4];
 	int count;
+
 	cwiid_wiimote_t *wiimote = NULL;
+
 	uint8_t ledstate = 0;
 	uint8_t ledno;
+
+	struct cwiid_state state;
 
 	if ((wiimote = cwiid_open(BDADDR_ANY, 0)) == NULL) {
 		fputs("Unable to connect", stderr);
 		return EXIT_FAILURE;
 	}
-	fputs("connected", stdout);
+	fputs("connected\n", stdout);
+
+	sleep(1);
+
+	if (!cwiid_get_state(wiimote, &state))
+		printf("battery at %d%%\n",
+			(int)(100.0 * state.battery / CWIID_BATTERY_MAX));
 	
 	while ((count = read(0, buf, 4)))
 	{
